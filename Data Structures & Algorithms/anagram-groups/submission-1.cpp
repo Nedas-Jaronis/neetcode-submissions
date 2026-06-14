5a8a47689a4aa876286b3e169a94d4153ba87c63
Nedas Jaronis <148506545+Nedas-Jaronis@users.noreply.github.com>
2026-06-14 12:14:49 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> result;
+        unordered_map<string,vector<string>> groups;
+
+        
+        for (string str : strs) {
+            string key = str;
+            sort(key.begin(), key.end());
+            groups[key].push_back(str);
+        }
+
+        for(auto& pair : groups){
+            result.push_back(pair.second);
+        }
+
+        return result;
+    }
+};
