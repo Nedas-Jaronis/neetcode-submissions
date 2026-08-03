4ceaf539bf1147ba2b5faab365381f27b6f4e88a
Nedas Jaronis <148506545+Nedas-Jaronis@users.noreply.github.com>
2026-08-03 11:58:00 -0400

    Add: string-compression - submission-0

---
+++ Data Structures & Algorithms/string-compression/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int write = 0, read = 0;
+        while(read < chars.size()){
+            char c = chars[read];
+            int runStart = read;
+            while(read < chars.size() && chars[read] == c){
+                read++;
+            }
+            int len = read - runStart;
+            chars[write++] = c;
+            if(len > 1){
+                for(char d : to_string(len)){
+                    chars[write++] = d;
+                }
+            }
+        }
+
+        return write;
+    }
+};
