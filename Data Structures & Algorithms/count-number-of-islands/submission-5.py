14973f92be4ffa2495bf351c6f8c2abb85ef8514
Nedas Jaronis <148506545+Nedas-Jaronis@users.noreply.github.com>
2026-08-13 11:35:22 -0400

    Add: count-number-of-islands - submission-5

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-5.py
@@ -0,0 +1,28 @@
+class Solution:
+
+    def sink(self, grid, row, col):
+        if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or grid[row][col] != '1':
+            return
+
+        grid[row][col] = '0'
+
+
+        self.sink(grid, row-1, col)
+        self.sink(grid, row+1, col)
+        self.sink(grid, row, col-1)
+        self.sink(grid, row, col+1)
+        
+
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if(grid[row][col]) == '1':
+                    count += 1
+                    self.sink(grid, row, col)
+    
+        return count
+
+
+
